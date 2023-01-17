a3ede190dbfe3433c1b89bc2227012d73bb608fa
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2023-01-17 13:21:26 +0000

    Correct __CHERI_PURE_CAPABILITY__ conditional.

--- include/v8-internal.h
+++ include/v8-internal.h
@@ -417,7 +417,7 @@ class Internals {
   static const int kIsolateLongTaskStatsCounterOffset =
       kIsolateFastApiCallTargetOffset + kApiSystemPointerSize;
   static const int kIsolateRootsOffset =
-#if defined(__CHER_PURE_CAPABILITY__)
+#if defined(__CHERI_PURE_CAPABILITY__)
       // With CHERI's stronger alignment, padding of sizeof(size_t) is added
       // between the long_stat_task_counter_ (size_t) and roots_table_ (uintptr_t)
       // fields.

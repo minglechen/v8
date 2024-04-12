f0732c67d83d1eec030ff82dfc5ed1264a18cc8e
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-04-12 10:12:23 +0100

    Apply patches to v8 from FreeBSD port.

--- src/base/cpu.cc
+++ src/base/cpu.cc
@@ -581,6 +581,7 @@ CPU::CPU()
 
 #if V8_OS_LINUX
 
+#if V8_OS_LINUX
   CPUInfo cpu_info;
 
   // Extract implementor from the "CPU implementer" field.
@@ -614,6 +615,7 @@ CPU::CPU()
     }
     delete[] part;
   }
+#endif
 
   // Extract architecture from the "CPU Architecture" field.
   // The list is well-known, unlike the the output of
--- src/d8/d8-posix.cc
+++ src/d8/d8-posix.cc
@@ -4,6 +4,8 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -12,7 +14,6 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
